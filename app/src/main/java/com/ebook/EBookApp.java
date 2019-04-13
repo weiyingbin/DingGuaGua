c93a7a22e02b75938c436ea7069b91ce8802520b
WEIYINGBIN <842777560@qq.com>
2019-04-13 10:54:44 +0800

    修改目标sdk为26

--- app/src/main/java/com/ebook/EBookApp.java
+++ app/src/main/java/com/ebook/EBookApp.java
@@ -3,6 +3,6 @@ package com.ebook;
 import android.app.Application;
 
 public class EBookApp extends Application {
-    public static final String urlApi = "http://192.168.1.110:8000";//后台api
+    public static final String urlApi = "http://47.99.204.34:8080/";//后台api
     public static final String urlBook = "https://www.qu.la";//小说网站
 }
